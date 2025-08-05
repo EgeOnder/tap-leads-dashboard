555566db519d0764d779dafa783fb6255869f3cd
Ege Önder <40398628+EgeOnder@users.noreply.github.com>
2025-08-05 18:00:09 +0200

    fix: add trusted origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,10 @@ import { admin as adminPlugin } from 'better-auth/plugins';
 import { db } from './db';
 
 export const auth = betterAuth({
+	trustedOrigins: [
+		'http://localhost:8080',
+		'https://j4m8r2x6w1v9z5q3t7n.direct-search.org',
+	],
 	database: drizzleAdapter(db, {
 		provider: 'pg',
 		usePlural: true,
